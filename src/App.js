c84476a2895fc58444b6817ae92a0e5692be209c
Shannon TJ <shannontj@shaw.ca>
2020-12-30 19:51:30 -0700

    define all compound exports from jumbotron

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ function App() {
       <Jumbotron.Container>
         {jumboData.map((data) => (
           <Jumbotron key={data.id} direction={data.direction}>
-            <p>HII</p>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{data.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{data.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={data.image} alt={data.alt} />
+            </Jumbotron.Pane>
           </Jumbotron>
         ))}
       </Jumbotron.Container>
