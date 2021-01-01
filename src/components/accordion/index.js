0b44e6277a35b7a9b36acef08c8be67446eb9920
Shannon TJ <shannontj@shaw.ca>
2020-12-31 18:03:56 -0700

    accordion setup

---
+++ src/components/accordion/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
+};
