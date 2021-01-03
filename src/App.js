f113059bd5be86edf43782baed6758619212edd6
Shannon TJ <shannontj@shaw.ca>
2021-01-03 00:27:30 -0700

    implement router with home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
 import * as ROUTES from "./constants/routes";
+import { Home } from "./pages";
 
 function App() {
   return (
     <Router>
       <Route exact path={ROUTES.HOME}>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <Home />
       </Route>
     </Router>
   );
