ddd09e820f41347103cf832aacc434497b750702
Shannon TJ <shannontj@shaw.ca>
2021-01-03 18:14:50 -0700

    setting up signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
+import { Form } from "../components";
 
 const Signin = () => {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  //check form input elements are valid (email and password)
+
   return (
     <>
-      <HeaderContainer />
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
