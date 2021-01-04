0cc7c5f829137b24b882144b6728800b45af450b
Shannon TJ <shannontj@shaw.ca>
2021-01-03 17:44:56 -0700

    seeding database and react firebase context

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -2,9 +2,9 @@ import Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-//seed the database
-
 //config
 const config = {};
 
 const firebase = Firebase.initializeApp(config);
+
+export { firebase };
