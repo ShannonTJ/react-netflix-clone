d87516865d625ffd6443bd8570501083b63ec76c
Shannon TJ <shannontj@shaw.ca>
2021-01-05 13:18:48 -0700

    profiles styling

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,11 +1,27 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
 
-  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+  //profile starts empty
+  //change loading to false when firebase data has been loaded
+  useEffect(() => {
+    console.log("profile", profile);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    loading ? (
+      <Loading src={user.photoURL} />
+    ) : null
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
