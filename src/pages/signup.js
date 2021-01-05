b03b6ec15c04838e66d0b59809b6c87a1b7562fc
Shannon TJ <shannontj@shaw.ca>
2021-01-04 22:04:19 -0700

    signup page built

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -25,19 +25,43 @@ const Signup = () => {
   };
 
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign Up</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
-        <Form.Base onSubmit={handleSignUp} method="POST">
-          <Form.Input
-            placeholder="First Name"
-            value={firstName}
-            onChange={({ target }) => setFirstName(target.value)}
-          />
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              placeholder="First name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+            <Form.Text>
+              Already a user?
+              <Form.Link to={ROUTES.SIGN_IN}> Sign in now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot. Learn more.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 };
 
