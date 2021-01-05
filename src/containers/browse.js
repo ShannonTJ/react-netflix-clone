6f53cdd8f7d909561acdf476824d79fbc4b54926
Shannon TJ <shannontj@shaw.ca>
2021-01-05 12:35:45 -0700

    profile component setup

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { SelectProfileContainer } from "./profiles";
 
 export function BrowseContainer() {
-  return <p>browsecontainer</p>;
+  return <SelectProfileContainer />;
 }
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -4,5 +4,23 @@ import { Header } from "../components";
 import logo from "../logo.svg";
 
 export function SelectProfileContainer({ user, setProfile }) {
-  return <p>selection profile</p>;
+  return (
+    <>
+      <Header bg={false}>
+        <Header.Frame>
+          <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+        </Header.Frame>
+      </Header>
+
+      <Profiles>
+        <Profiles.Title>Who's watching?</Profiles.Title>
+        <Profiles.List>
+          <Profiles.User>
+            <Profiles.Picture src={user.photoURL} />
+            <Profiles.Name>{user.displayName}</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
+    </>
+  );
 }
