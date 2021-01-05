1086b2c201037e518adfd24fb1fe4900c6199b92
Shannon TJ <shannontj@shaw.ca>
2021-01-04 21:56:09 -0700

    building signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { Form, Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import { useHistory } from "react-router-dom";
 
 const Signup = () => {
-  return <p>hello from sign up</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  //check form input elements are valid (email and password)
+  const isInvalid = firstName === "" || password === "" || emailAddress === "";
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    //firebase stuff
+  };
+
+  return (
+    <HeaderContainer>
+      <Form>
+        <Form.Title>Sign Up</Form.Title>
+        {error && <Form.Error>{error}</Form.Error>}
+        <Form.Base onSubmit={handleSignUp} method="POST">
+          <Form.Input
+            placeholder="First Name"
+            value={firstName}
+            onChange={({ target }) => setFirstName(target.value)}
+          />
+        </Form.Base>
+      </Form>
+    </HeaderContainer>
+  );
 };
 
 export default Signup;
